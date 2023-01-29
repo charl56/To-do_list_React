3d8bf8e0bd066b440d9a012f59542ae10f4a08fb
pichouch <charles.pichou@reseau.eseo.fr>
2023-01-29 18:32:30 +0100

    log db, readme

--- backend/log/log.py
+++ backend/log/log.py
@@ -1,22 +1,28 @@
 import mysql.connector
-from dotenv import dotenv_values
-
+from dotenv import dotenv_values, load_dotenv
+import os
+from os.path import join, dirname
 
 def login_database(): 
     
     try: 
-        config = dotenv_values(".env")
+        print("config dotenv")
 
-        username = config.get("username")
-        password = config.get("password")
+        dotenv_path = join(dirname(__file__), '.env')
+        print(dotenv_path)
+        load_dotenv(dotenv_path)
+
+
+        username = os.environ.get("username-db")
+        password = os.environ.get("password-db")
         print(username, password)
 
    
         conn = mysql.connector.connect(
             host="localhost",
             user=username,
-            password=password,
-            database="apptaches"
+            password=password
+            # database="apptaches"
         )
         return conn
     except ValueError:
